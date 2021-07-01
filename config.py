7b3f208a036613b0e0ec7b2fe6e69b22e30ba781
Anonymous Guy <anonguy912@gmail.com>
2021-07-01 22:37:12 +0530

    initial commit

---
+++ config.py
@@ -0,0 +1,10 @@
+import  os
+
+BOT_TOKEN = os.environ.get("1849971418:AAF9zK_c6c5ayyX0XbE00BDm64GLMCF3biQ")
+APP_ID = int(os.environ.get("1378165"))
+API_HASH = os.environ.get("dd2e80c7745fa709a04842c081db3bc8")
+
+youtube_next_fetch = 0  # time in minute
+
+
+EDIT_TIME = 5
