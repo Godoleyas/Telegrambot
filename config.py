7a9980f2639b8517ef0dffd1c0192ff7637d9ff5
Godoleyas <69921474+Godoleyas@users.noreply.github.com>
2021-07-04 08:50:36 +0300

    Update

--- config.py
+++ config.py
@@ -10,10 +10,10 @@ def init_log() -> int:
 
 
 class Config:
-    BOT_TOKEN = os.environ["1728087210:AAFj2wMxr_TFOYdASuy1NRJh3OFOHfvto5Y"]
-    API_ID = int(os.environ["1378165"])
-    API_HASH = os.environ["dd2e80c7745fa709a04842c081db3bc8"]
+    BOT_TOKEN = os.environ["BOT_TOKEN"]
+    API_ID = int(os.environ["API_ID"])
+    API_HASH = os.environ["API_HASH"]
     EXEC_PATH = os.environ.get("GOOGLE_CHROME_SHIM", None)
     # OPTIONAL
     LOG_GROUP = init_log()
-    SUPPORT_GROUP_LINK = os.environ.get("SUPPORT_GROUP", "https://t.me/webdevchat")
+    SUPPORT_GROUP_LINK = os.environ.get("SUPPORT_GROUP", "SUPPORT_GROUP_LINK")
